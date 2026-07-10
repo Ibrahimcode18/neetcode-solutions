07739098150d4eafe8842cd31f92c9098e6ab010
Ibrahimcode18 <ibfatade18@gmail.com>
2026-07-10 23:47:47 +0100

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        close = [')', '}', ']']
+        hash_map = {')': '(', '}' : '{', ']': '['}
+        for i in range(len(s)):
+            if s[i] in close and len(stack) != 0:
+                if stack[-1] == hash_map[s[i]]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(s[i])
+        if len(stack) != 0:
+            return False
+        return True
